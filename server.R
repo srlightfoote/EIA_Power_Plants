f987a1b9a6fa2645b3e1ca9a4cf63f1b60ca5aae
Stephen Lightfoote <srlightfoote@gmail.com>
2017-05-09 14:52:42 -0400

    Add files via upload

---
+++ server.R
@@ -0,0 +1,180 @@
+#### query EIA function ####
+queryEIA<-function(plantID,baseID,key){
+  eia.xts<-getEIA(ID = str_replace(baseID,'eiaid',as.character(plantID)),key = key)
+}
+
+#define EIA key + data channel
+key<-'743E7EEB9DD74A8FBD876F83F1196F8D'
+baseID<-'ELEC.PLANT.GEN.eiaid-ALL-ALL.M'
+
+#### import EIA data ####
+plants.dt<-data.table(read.csv('eia_powerplants_US.csv'))
+
+#### shiny server function ####
+shinyServer(function(input, output,session) {
+  activeData<-reactiveValues(eia.xts=FALSE,
+                             selected_plant.dt=FALSE)
+  
+  #reactive controls
+  basemap<-reactive({input$basemap})
+  showLabels<-reactive({input$labels})
+  allowEIA<-reactive({input$dataset})
+  
+  #reactive dataset
+  dataset <- reactive({
+    plants.dt[is.element(plants.dt$primsource,input$dataset) &
+                plants.dt$total_mw>=input$capRange[1] &
+                plants.dt$total_mw<=input$capRange[2],]
+  })
+  
+  #### Interactive Map ####
+  #initialize map
+  output$map<-renderLeaflet({
+    leaflet(data = plants.dt) %>%
+      fitBounds(~min(longitude,na.rm = TRUE), ~min(latitude,na.rm = TRUE),
+                ~max(longitude,na.rm = TRUE), ~max(latitude,na.rm = TRUE)) %>%
+      addMeasure(position='topleft',primaryLengthUnit = 'kilometers',secondaryLengthUnit = 'miles')%>%
+      addEasyButton(easyButton(
+        icon="fa-globe", title="Zoom to Level 3",
+        onClick=JS("function(btn, map){ map.setZoom(3); }")))
+  })
+  
+  #initialize Data Table
+  output$Table<-renderDataTable(dataset(),filter="top",options = list(pageLength = 10),selection = 'single')
+  
+  #Data Controls
+  observe({
+    pal <- colorFactor(input$colors, dataset()$primsource)
+    data.dt<-dataset()
+    labels <- sprintf(
+      "<strong>%s</strong><br/>Generator: %s<br/>Capacity: %g MW",
+      data.dt$plant_name, data.dt$primsource,data.dt$total_mw
+    ) %>% lapply(htmltools::HTML)
+    
+    leafletProxy("map", data=data.dt,session=session) %>%
+      clearShapes() %>%
+      clearControls() %>%
+      addCircles(~longitude,~latitude, weight = 1,radius=~(log(total_mw)*1000),
+                 fillColor =~pal(primsource),color = "#777777",
+                 stroke = TRUE, fillOpacity = .7,
+                 highlightOptions = highlightOptions(color = "black", weight = 3,
+                                                     bringToFront = TRUE),
+                 label=labels,
+                 labelOptions = labelOptions(
+                   style = list("font-weight" = "normal", padding = "3px 8px"),
+                   textsize = "15px",
+                   direction = "auto")
+      ) %>% 
+      addLegend(position = "bottomleft",
+                pal = pal, values = ~primsource,title = "Generator Type")
+  })
+  
+  #Add/ Remove Data Labels
+  observe({
+    showLabel<-showLabels()
+    if (showLabel==TRUE) {
+      data_in_extent<- dataset()
+      data_in_extent<-with(data_in_extent,data_in_extent[latitude>input$map_bounds$south &
+                                                           latitude<input$map_bounds$north &
+                                                           longitude>input$map_bounds$west &
+                                                           longitude<input$map_bounds$east,])
+      leafletProxy("map", data = data_in_extent) %>% 
+        clearPopups() %>% 
+        addPopups(lng = ~longitude,lat = ~latitude,popup = ~plant_name)
+    } else{
+      leafletProxy("map") %>% clearPopups() 
+    }
+  })
+  
+  #change basemap
+  observe({
+    tile<-basemap()
+    data.dt<-dataset()
+    leafletProxy("map") %>% clearTiles() %>% addProviderTiles(provider = tile)
+  })
+  
+  #update selected plant if map point is clicked
+  observeEvent(eventExpr = input$map_shape_click,{
+    selected_plant.dt<-dataset()[round(dataset()$latitude,3)==round(input$map_shape_click$lat,3) & 
+                                   round(dataset()$longitude,3)==round(input$map_shape_click$lng,3),]
+    activeData$selected_plant.dt<-selected_plant.dt
+    
+    #update map
+    leafletProxy("map",data=selected_plant.dt) %>%
+      fitBounds(~min(longitude,na.rm = TRUE)-.1, ~min(latitude,na.rm = TRUE)-.1,
+                ~max(longitude,na.rm = TRUE)+.1, ~max(latitude,na.rm = TRUE)+.1)
+    
+    #update data table
+    data.dt<-dataset()
+    Table.dt<-data.dt
+    selected<-which(Table.dt$plant_code==selected_plant.dt$plant_code)
+    output$Table <- DT::renderDataTable({
+      DT::datatable(Table.dt,filter="top",options = list(pageLength = 10,displayStart = selected-1),selection = list(mode = "single", target = "row", selected = selected) )
+    })
+    
+    #query EIA
+    if(input$queryEIA){
+      if(sum(as.numeric(selected_plant.dt$plant_code),na.rm=TRUE)>0){
+        #query the EIA Data
+        eia.xts<-queryEIA(selected_plant.dt$plant_code,baseID,key)
+        activeData$eia.xts<-eia.xts
+        #show mini-plot in map
+        output$eiaGen<-renderPlot({
+          plot(eia.xts,main =selected_plant.dt$plant_name,
+               ylab = 'MWh',cex.main=.75,cex.axis=.5,cex.lab=.5)
+        })
+        output$eiaMonthlyGen<-renderDygraph(
+          dygraph(eia.xts,main=selected_plant.dt$plant_name,
+                  xlab = 'Date',ylab = 'MWh') %>% 
+            dyOptions(axisLabelFontSize = 10)  %>% 
+            dyRoller(rollPeriod = 1)
+        )
+      }
+    }
+  })
+  
+  #update selected plant if table row is selected
+  observeEvent(eventExpr = input$Table_rows_selected,{
+    selected_plant.dt<-dataset()[input$Table_rows_selected,]
+    activeData$selected_plant.dt<-selected_plant.dt
+    
+    #zoom to plant location
+    leafletProxy("map",data=selected_plant.dt) %>%
+      fitBounds(~min(longitude,na.rm = TRUE)-.1, ~min(latitude,na.rm = TRUE)-.1,
+                ~max(longitude,na.rm = TRUE)+.1, ~max(latitude,na.rm = TRUE)+.1)
+    
+    #update data table
+    data.dt<-dataset()
+    Table.dt<-data.dt
+    selected<-which(Table.dt$plant_code==selected_plant.dt$plant_code)
+    output$Table <- DT::renderDataTable({
+      DT::datatable(Table.dt,filter="top",options = list(pageLength = 10,displayStart = selected-1),selection = list(mode = "single", target = "row", selected = selected) )
+    })
+    
+    #query EIA
+    if(input$queryEIA){
+      if(sum(as.numeric(selected_plant.dt$plant_code),na.rm=TRUE)>0){
+        #query the EIA Data
+        eia.xts<-queryEIA(selected_plant.dt$plant_code,baseID,key)
+        activeData$eia.xts<-eia.xts
+        #show mini-window in map
+        output$eiaGen<-renderPlot({
+          plot(eia.xts,main = paste(selected_plant.dt$plant_name,' Net MWh',sep=''),ylab = 'MWh')
+        })
+        output$eiaMonthlyGen<-renderDygraph(
+          dygraph(eia.xts,main=selected_plant.dt$plant_name,
+                  xlab = 'Date',ylab = 'MWh') %>% 
+            dyOptions(axisLabelFontSize = 10)  %>% 
+            dyRoller(rollPeriod = 1)
+        )
+      }
+    }
+  })
+  
+  #export EIA data to CSV
+  output$exportEIA <- downloadHandler(
+    filename = function() { paste(activeData$selected_plant.dt$plant_name,'_EIA_data.csv',sep='')},
+    content = function(con) { write.csv(as.data.table(activeData$eia.xts), con,row.names = FALSE)}
+  )
+  
+})
---
+++ ui.R
@@ -0,0 +1,60 @@
+#libraries
+library(data.table)
+library(DT)
+library(dygraphs)
+library(EIAdata)
+library(leaflet)
+library(RColorBrewer)
+library(shiny)
+library(stringr)
+
+shinyUI(
+  navbarPage(
+    "EIA Power Plant Explorer", id="nav",
+    tabPanel(
+      "Map",
+      div(class="outer",
+          tags$style(type = "text/css", ".outer {font-size:10px; position: fixed; top: 41px; left: 0; right: 0; bottom: 0; overflow: hidden; padding: 0}"),
+          leafletOutput("map", width = "100%", height = "100%"),
+          absolutePanel(fixed = TRUE,draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+                        width = 200, height = 550,style ="padding: 8px; border-bottom: 1px solid #CCC; background: #FFFFFF; opacity: 0.8;",
+                        selectInput('basemap','Basemap',choices = 
+                                      c("CartoDB.Positron","CartoDB.DarkMatter", "Esri","Esri.WorldStreetMap","Esri.DeLorme","Esri.WorldTopoMap","Esri.WorldImagery",
+                                        "Esri.WorldTerrain","Esri.WorldShadedRelief","Esri.WorldPhysical","Esri.OceanBasemap","Esri.NatGeoWorldMap" )
+                                    ,selected = "CartoDB.Positron"),
+                        selectInput("dataset",label = "Generator Type(s)",choices = c("hydroelectric",
+                                                                                      "natural gas",
+                                                                                      "pumped storage",
+                                                                                      "petroleum",
+                                                                                      "geothermal",
+                                                                                      "solar",
+                                                                                      "wind",
+                                                                                      "coal",
+                                                                                      "nuclear",
+                                                                                      "biomass",
+                                                                                      "other"),selected = c("hydroelectric","solar","wind"),multiple=TRUE),
+                        sliderInput(inputId = "capRange","Plant Capacity [MW]",min = 0,max=10000,value = c(0,10000),step=1),
+                        selectInput("colors", "Color Scheme",
+                                    c('viridis',rownames(subset(brewer.pal.info, category %in% c("seq", "div")))),
+                                    selected ='viridis'),
+                        checkboxInput('labels','Show labels'),
+                        checkboxInput('queryEIA','Query EIA Data',value = TRUE),
+                        plotOutput("eiaGen",height = 150)
+          )
+      )
+    ),
+    tabPanel('Data',
+             sidebarLayout(
+               sidebarPanel(
+                 downloadButton('exportEIA','Export EIA Data'),
+                 h4(),
+                 dygraphOutput("eiaMonthlyGen",height = 400)
+               ),
+               mainPanel(
+                 div(DT::dataTableOutput("Table"), style = "font-size: 75%; width: 75%")
+                 
+               )
+             )
+    )
+  )
+)
