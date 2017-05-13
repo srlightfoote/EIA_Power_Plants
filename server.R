bcc6e1f0b856e9d115827c85fcda0065df9a57c0
Stephen Lightfoote <srlightfoote@gmail.com>
2017-05-13 09:41:46 -0400

    Update server.R

--- server.R
+++ server.R
@@ -4,7 +4,7 @@ queryEIA<-function(plantID,baseID,key){
 }
 
 #define EIA key + data channel
-key<-'743E7EEB9DD74A8FBD876F83F1196F8D'
+key<-'<paste your key here>'
 baseID<-'ELEC.PLANT.GEN.eiaid-ALL-ALL.M'
 
 #### import EIA data ####
